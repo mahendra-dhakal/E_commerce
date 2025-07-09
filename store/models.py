5aa984439f0de3ba6fab04a7b05eb8e89ba2514d
Mahendra Dhakal <mahendradhakal700@gmail.con>
2025-07-09 08:15:46 +0545

    stocks models created and registered in admin.py

--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from category.models import Category
 # Create your models here.
 
 
@@ -10,4 +10,11 @@ class Product(models.Model):
     price = models.IntegerField()
     image = models.FileField(upload_to='photos/products')
     stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     
+    def __str__(self):
+        return self.product_name
